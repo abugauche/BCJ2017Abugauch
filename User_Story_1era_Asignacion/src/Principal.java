0a5204b15d65b98374530e7e40469954d33e8da3
abugauche <e.abugauch1@gmail.com>
2017-01-13 01:59:03 -0300

    Last details

--- User_Story_1era_Asignacion/src/Principal.java
+++ User_Story_1era_Asignacion/src/Principal.java
@@ -2,88 +2,92 @@
  * Created by Usuario on 11/1/2017.
  */
 
-import java.util.Scanner;
+import java.util.*;
 public class Principal {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sd = new Scanner(System.in);
         System.out.println("----------WELCOME US01 BCJAbugauch----------");
         System.out.println("Dou you want to enter? [Y/N]");
-        while (sc.hasNext() && (sc.nextLine().equalsIgnoreCase("y"))) {//change here
+        while (sd.hasNext() && (sd.nextLine().equalsIgnoreCase("y"))) {
             System.out.println("Option: 1) Load data 2) Check DB Connection 99)Exit)");
-            int opcion = sc.nextInt();
+            int opcion = sd.nextInt();
             switch(opcion){
                 case 1: {
+                    Scanner sc = new Scanner(System.in);
+
                     System.out.println("Country");
-                    System.out.print("Ingrese el nombre del country: ");
+                    System.out.println("Enter country name: ");
                     String name  = sc.nextLine();
-                    System.out.println("Ingrese el alpa2_code del country: ");
+                    System.out.println("Two-digit country code: ");
                     String alpa2_code = sc.nextLine();
-                    System.out.println("Ingrese el alpa3_code del country: ");
+                    System.out.println("Three-digit country code: ");
                     String alpa3_code = sc.nextLine();
                     Country country = new Country(name, alpa2_code, alpa3_code);
-                    System.out.println("State");
-                    String res = "No se cargo ningun estado;";
-                    System.out.println("Nombre del estado: ");
-                    String nameS = sc.nextLine();
-                    System.out.println("Abreviatura del estado " + nameS + ": ");
-                    String abbr = sc.nextLine();
-                    System.out.println("Area del estado " + nameS + ": ");
-                    int area = sc.nextInt();
-                    System.out.println("Capital del estado " + nameS + ": ");
-                    String capital = sc.nextLine();
 
+                    System.out.println("State");
+                    System.out.println("Name: ");
+                    String nameS = sc.nextLine();
+                    System.out.println("Abbreviation: ");
+                    String abbr = sc.nextLine();
+                    System.out.println("Capital: ");
+                    String capital = sc.nextLine();
+                    System.out.println("Area: ");
+                    int area = sc.nextInt();
                     State state = new State(nameS, abbr, area, capital, country);
+
                     System.out.println("Wind");
                     System.out.println("Id Wind: ");
                     int idWind = sc.nextInt();
                     System.out.println("Chill: ");
                     int chill = sc.nextInt();
-                    System.out.println("direction: ");
+                    System.out.println("Direction: ");
                     int direction = sc.nextInt();
-                    System.out.println("speed: ");
+                    System.out.println("Speed: ");
                     int speed = sc.nextInt();
                     Wind wind = new Wind(idWind, chill, direction, speed);
+
                     System.out.println("Atmosphere");
-                    System.out.println("idAtmosphere: ");
+                    System.out.println("IdAtmosphere: ");
                     int idAtmosphere = sc.nextInt();
-                    System.out.println("humidity: ");
+                    System.out.println("Humidity: ");
                     int humidity = sc.nextInt();
-                    System.out.println("presseure: ");
+                    System.out.println("Presseure: ");
                     float presseure = sc.nextFloat();
-                    System.out.println("rising: ");
+                    System.out.println("Rising: ");
                     int rising = sc.nextInt();
-                    System.out.println("visibility: ");
+                    System.out.println("Visibility: ");
                     float visibility = sc.nextFloat();
                     Atmosphere atmosphere = new Atmosphere(idAtmosphere, humidity, presseure, rising, visibility);
+
                     System.out.println("Weather");
-                    System.out.println("idWeather");
+                    System.out.println("Description: ");
+                    String description = sc.nextLine();
+                    System.out.println("IdWeather: ");
                     int idWeather = sc.nextInt();
-                    System.out.println("descr");
-                    String descr = sc.nextLine();
-                    Weather weather = new Weather(idWeather, descr);
-                    System.out.println(weather.toString());
-                    System.out.println("Condition");
-                    System.out.println("actually date: ");
+                    Weather weather = new Weather(idWeather, description);
+
+                    System.out.println("Condition ");
+                    System.out.println("Actually date: ");
                     String date = sc.nextLine();
-                    System.out.println("temp: ");
+                    System.out.println("Temperature: ");
                     int temp = sc.nextInt();
                     Condition condition = new Condition(date, temp, state, weather, atmosphere, wind);
                     System.out.println(condition.toString());
-                    System.out.println("Extended");
-                    ExtendedCondition  [] extendedCondition = new ExtendedCondition[9];
+
+                    System.out.println("Extended Condition");
+                    ExtendedCondition  [] extendedCondition = new ExtendedCondition[10];
                     for(int i=0; i<extendedCondition.length;i++){
-                        System.out.println("Extended Condition day "+i+": ");
+                        System.out.println("------Day " + (i+1)+"------");
+                        System.out.println("Weather description: ");
+                        String description1 = sc.nextLine();
                         System.out.println("Id Weather: ");
                         int idWeather1 = sc.nextInt();
-                        System.out.println("Weather description: ");
-                        String descr1 = sc.nextLine();
-                        Weather weather1 = new Weather(idWeather1,descr1);
-                        System.out.println("low");
+                        Weather weather1 = new Weather(idWeather1,description1);
+                        System.out.println("Low: ");
                         int low = sc.nextInt();
-                        System.out.println("high");
+                        System.out.println("High: ");
                         int high = sc.nextInt();
-                        System.out.println("Day " + i);
                         extendedCondition [0] = new ExtendedCondition(i, high, low, condition, weather1);
                     }
                     for (int i=0;i<extendedCondition.length;i++){
